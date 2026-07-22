e84fc38302470ede8a3a7beee0ec70f807749736
dev-spemto <305669279+dev-spemto@users.noreply.github.com>
2026-07-22 09:45:41 +0700

    Fix duplicate role migration and update user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -40,6 +40,8 @@ class DatabaseSeeder extends Seeder
 
                 'nama' => 'Administrator',
 
+                'username' => 'admin',
+
                 'password' => Hash::make('admin123'),
 
                 'role' => 'admin',
